c1e9946d2a77bdae35a6be500d43446d92857c3d
Atmozo <ashelymozorand@gmail.com>
2024-08-24 20:49:45 +0200

    register and login

--- src/test/java/com/automation/pages/RegisterPage.java
+++ src/test/java/com/automation/pages/RegisterPage.java
@@ -1,7 +1,7 @@
 package com.automation.pages;
 
+import com.automation.utilities.ScreenShot;
 import io.cucumber.java.Scenario;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,23 +9,109 @@ import org.openqa.selenium.support.How;
 
 public class RegisterPage extends BasePage {
 
-    @FindBy (how = How.NAME, using = "firstname")
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.firstName\"]")
     private WebElement txtEnterFirstName;
 
-    private By lastName;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.lastName\"]")
+    private WebElement txtEnterLastName;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.address.street\"]")
+    private WebElement txtEnterAddress;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.address.city\"]")
+    private WebElement txtEnterCity;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.address.state\"]")
+    private WebElement txtEnterState;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.address.zipCode\"]")
+    private WebElement txtEnterZipCode;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.phoneNumber\"]")
+    private WebElement txtEnterPhoneNumber;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.ssn\"]")
+    private WebElement txtEnterSSN;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.username\"]")
+    private WebElement txtEnterUsername;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"customer.password\"]")
+    private WebElement txtEnterPassword;
+
+    @FindBy(how = How.XPATH, using = "//*[@id=\"repeatedPassword\"]")
+    private WebElement txtEnterConfirmPassword;
+
+    @FindBy(how = How.XPATH, using = "//input[@value='Register']")
+    private WebElement btnRegister;
 
     public RegisterPage(WebDriver driver, Scenario scenario) {
         super(driver, scenario);
     }
 
-
     @Override
     public boolean isAt() {
-        return false;
+        return txtEnterFirstName.isDisplayed();
     }
 
-    public void EnterFirstName(String name){
+    public void enterFirstName(String name) {
         txtEnterFirstName.clear();
-        txtEnterFirstName.sendKeys();
+        txtEnterFirstName.sendKeys(name);
+    }
+
+    public void enterLastName(String lastName) {
+        txtEnterLastName.clear();
+        txtEnterLastName.sendKeys(lastName);
+    }
+
+    public void enterAddress(String address) {
+        txtEnterAddress.clear();
+        txtEnterAddress.sendKeys(address);
+    }
+
+    public void enterCity(String city) {
+        txtEnterCity.clear();
+        txtEnterCity.sendKeys(city);
+    }
+
+    public void enterState(String state) {
+        txtEnterState.clear();
+        txtEnterState.sendKeys(state);
+    }
+
+    public void enterZipCode(String zipCode) {
+        txtEnterZipCode.clear();
+        txtEnterZipCode.sendKeys(zipCode);
+    }
+
+    public void enterPhoneNumber(String phone) {
+        txtEnterPhoneNumber.clear();
+        txtEnterPhoneNumber.sendKeys(phone);
+    }
+
+    public void enterSSN(String ssn) {
+        txtEnterSSN.clear();
+        txtEnterSSN.sendKeys(ssn);
+    }
+
+    public void enterUsername(String username) {
+        txtEnterUsername.clear();
+        txtEnterUsername.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        txtEnterPassword.clear();
+        txtEnterPassword.sendKeys(password);
+    }
+
+    public void enterConfirmPassword(String confirmPassword) {
+        txtEnterConfirmPassword.clear();
+        txtEnterConfirmPassword.sendKeys(confirmPassword);
+    }
+
+    public void clickRegisterButton() {
+        ScreenShot.captureScreenShot(scenario);
+        btnRegister.click();
+
     }
 }
--- src/test/java/com/automation/stepdefinitions/RegisterStepdefs.java
+++ src/test/java/com/automation/stepdefinitions/RegisterStepdefs.java
@@ -1,47 +1,120 @@
 package com.automation.stepdefinitions;
 
-import com.automation.pages.LoginPage;
+
 import com.automation.pages.RegisterPage;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
-import io.cucumber.java.it.Ma;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.Map;
 
 import static com.automation.constants.Constants.driver;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 
 public class RegisterStepdefs {
 
     RegisterPage registerPage;
-    @BeforeStep
-    public void startup(Scenario scenario){
 
-        registerPage = new RegisterPage( driver,scenario);
+    @BeforeStep
+    public void startup(Scenario scenario) {
+        registerPage = new RegisterPage(driver, scenario);
     }
 
-
+    @Given("I am on the parabank website landing page")
+    public void iAmOnTheParabankWebsiteLandingPage() {
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
+    }
 
     @When("I click the register link")
     public void iClickTheRegisterLink() {
+        WebElement registerLink = new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.elementToBeClickable(By.linkText("Register")));
+        registerLink.click();
     }
 
-
     @And("I enter firstname as {string}")
-    public void iEnterFirstnameAs(String arg0, String arg1) {
+    public void iEnterFirstnameAs(String firstName) {
+        registerPage.enterFirstName(firstName);
+    }
 
+    @And("I enter lastname as {string}")
+    public void iEnterLastnameAs(String lastName) {
+        registerPage.enterLastName(lastName);
     }
 
     @And("I enter name as {string}")
-    public void iEnterNameAs(String arg0, String arg1) {
-
+    public void iEnterNameAs(DataTable data) {
+        Map<String, String> table = data.transpose().asMap(String.class, String.class);
+        registerPage.enterFirstName(table.get("first_name"));
+        registerPage.enterLastName(table.get("lastname"));
     }
 
-    @And("I enter name as {}")
-    public void iEnterNameAs(DataTable data) {
-        Map<String,String> table = data.transpose().asMap();
-        registerPage.EnterFirstName(table.get("first_name"));
+    @And("I enter address as {string}")
+    public void iEnterAddressAs(String address) {
+        registerPage.enterAddress(address);
+    }
+
+    @And("I enter city as {string}")
+    public void iEnterCityAs(String city) {
+        registerPage.enterCity(city);
+    }
+
+    @And("I enter state as {string}")
+    public void iEnterStateAs(String state) {
+        registerPage.enterState(state);
+    }
+
+    @And("I enter zipcode as {string}")
+    public void iEnterZipcodeAs(String zipcode) {
+        registerPage.enterZipCode(zipcode);
+    }
+
+    @And("I enter phone as {string}")
+    public void iEnterPhoneAs(String phone) {
+        registerPage.enterPhoneNumber(phone);
+    }
+
+    @And("I enter ssn as {string}")
+    public void iEnterSsnAs(String ssn) {
+        registerPage.enterSSN(ssn);
+    }
+
+    @And("I enter username as {string}")
+    public void iEnterUsernameAs(String username) {
+        registerPage.enterUsername(username);
+    }
+
+    @And("I enter password as {string}")
+    public void iEnterPasswordAs(String password) {
+        registerPage.enterPassword(password);
+    }
+
+    @And("I enter confirm as {string}")
+    public void iEnterConfirmAs(String confirm) {
+        registerPage.enterConfirmPassword(confirm);
+    }
+
+    @And("I click the register button")
+    public void iClickTheRegisterButton() {
+        registerPage.clickRegisterButton();
+    }
+
+    @And("the system confirms the registration")
+    public void theSystemConfirmsTheRegistration() {
+
+        String confirmationMessage = new WebDriverWait(driver, Duration.ofSeconds(15))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("title")))
+                .getText();
+        assertThat("Registration confirmation message is displayed", confirmationMessage, containsString("Welcome mavusana"));
     }
 }
